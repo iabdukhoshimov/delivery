1c1014dba5e1896125e1e1091bd006b6f669ff20
iabdukhoshimov <i.abdukhoshimov@gmail.com>
2022-12-21 17:57:23 +0500

    new service delivery

---
+++ telegraf/keyboards/home.js
@@ -0,0 +1,131 @@
+module.exports = {
+    ru: [
+        [ 
+            {
+                text: '📝 Онлайн заявка', 
+            }
+        ], 
+        [
+            {
+                text: '🏢 Список дилерских центров', 
+                web_app: { url: 'https://test.samauto.uz/products' }
+            } 
+        ], 
+        [ 
+            {
+                text: '📃 Список продукции компании',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [
+            {
+                text: '🚚 Автомобили в наличии',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [
+            {
+                text: '📣 Текущие акции компании',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [
+            {
+                text: '💵 Прайс лист'
+            }
+        ],
+        [
+            {
+                text: '📞 Контакты',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [ '🇷🇺 Сменить язык' ],
+    ],
+    en: [
+        [ 
+            {
+                text: '📝 Онлайн заявка', 
+            }
+        ], 
+        [
+            {
+                text: '🏢 Список дилерских центров', 
+                web_app: { url: 'https://test.samauto.uz/products' }
+            } 
+        ], 
+        [ 
+            {
+                text: '📃 Список продукции компании',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [
+            {
+                text: '🚚 Автомобили в наличии',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [
+            {
+                text: '📣 Текущие акции компании',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [
+            {
+                text: '💵 Прайс лист'
+            }
+        ],
+        [
+            {
+                text: '📞 Контакты',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [ '🇷🇺 Сменить язык' ],
+    ],
+    uz: [
+        [ 
+            {
+                text: '📝 Онлайн заявка', 
+            }
+        ], 
+        [
+            {
+                text: '🏢 Список дилерских центров', 
+                web_app: { url: 'https://test.samauto.uz/products' }
+            } 
+        ], 
+        [ 
+            {
+                text: '📃 Список продукции компании',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [
+            {
+                text: '🚚 Автомобили в наличии',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [
+            {
+                text: '📣 Текущие акции компании',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [
+            {
+                text: '💵 Прайс лист'
+            }
+        ],
+        [
+            {
+                text: '📞 Контакты',
+                web_app: { url: 'https://test.samauto.uz/products' }
+            }
+        ],
+        [ '🇷🇺 Сменить язык' ],
+    ],
+}
---
+++ telegraf/keyboards/request.js
@@ -0,0 +1,95 @@
+module.exports = {
+    ru: [
+        [
+            {
+                text: 'Заявка на тест-драйв',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: 'Заявка на продукцию',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: 'Заявка на запасные части',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: 'Заявка на сервис',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: '⬅️ Назад',
+            }
+        ]
+    ],
+    en: [
+        [
+            {
+                text: 'Заявка на тест-драйв',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: 'Заявка на продукцию',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: 'Заявка на запасные части',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: 'Заявка на сервис',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: '⬅️ Назад',
+            }
+        ]
+    ],
+    uz: [
+        [
+            {
+                text: 'Заявка на тест-драйв',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: 'Заявка на продукцию',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: 'Заявка на запасные части',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: 'Заявка на сервис',
+                web_app: { url: 'https://google.com' }
+            }
+        ],
+        [
+            {
+                text: '⬅️ Назад',
+            }
+        ]
+    ]
+}
