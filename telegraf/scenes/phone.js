6de6623a93d501eed5959deaeae1ffac5d51fa3f
iabdukhoshimov <i.abdukhoshimov@gmail.com>
2023-01-06 03:40:00 +0500

    error handling for the phone number

--- telegraf/scenes/phone.js
+++ telegraf/scenes/phone.js
@@ -2,21 +2,21 @@ const Telegraf = require('telegraf')
 const { Extra, Markup } = Telegraf
 const Scene = require('telegraf/scenes/base')
 const UserStorage = require('../../storage/mongo/user')
-const { phone_keyboard } = require('../keyboards/phone') 
-const { start_message } = require('../../helper/constants') 
+const { phone_keyboard } = require('../keyboards/phone')
+const { start_message } = require('../../helper/constants')
 
 class PhoneScene {
-    Phone () {
+    Phone() {
         const phone = new Scene('phone');
-        
-        phone.enter(async (ctx) => {
-             await ctx.reply(
-                start_message, 
+
+        phone.enter(async(ctx) => {
+            await ctx.reply(
+                start_message,
                 Markup
-                    .keyboard(phone_keyboard)
-                    .oneTime()
-                    .resize()
-                    .extra()
+                .keyboard(phone_keyboard)
+                .oneTime()
+                .resize()
+                .extra()
             )
             return
         })
@@ -30,13 +30,21 @@ class PhoneScene {
         phone.on('contact', async ctx => {
             const { user_id } = ctx.update.message.contact
             const { phone_number } = ctx.update.message.contact
-            
+
             const resp = await UserStorage.ChangePhone({ chat_id: user_id, phone: phone_number })
 
-            if(resp) {
+            if (resp) {
                 await ctx.scene.enter('lang')
                 return
             }
+
+            // error handling
+            if (!resp) {
+                await ctx.reply('Error')
+                await ctx.scene.reenter()
+                return
+            }
+
             await ctx.scene.reenter()
             return
         })
